16d05c53dcdf5a024375487f91c0afa770235e30
junydania <o.dania@aol.com>
2017-04-03 16:54:24 +0200

    Initial fizz buzz commit

--- lib/atm.rb
+++ lib/atm.rb
@@ -6,10 +6,18 @@ class Atm
     @funds = 1000
   end
 
-  def withdraw(amount)
-    @funds -= amount
-  end
-end
+  def withdraw(amount, account)
+    case
 
-class Account
+    when amount > account.balance then
+      return
+
+    else
+      @funds -= amount
+      account.balance -= amount
+
+      {:status =>true, :message => 'success', :date => Date.today, :amount => amount}
+      
+    end
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,4 +1,5 @@
 require './lib/atm.rb'
+require 'date'
 
 describe Atm do
   it 'has $1000 on initialize' do
@@ -6,7 +7,23 @@ describe Atm do
   end
 
   it 'funds are reduced at withdraw' do
-    subject.withdraw 50
+    subject.withdraw(50, account)
     expect(subject.funds).to eq(950)
   end
+
+
+let(:account) { instance_double('Account')}
+
+before do
+  allow(account).to receive(:balance).and_return(100)
+  allow(account).to receive(:balance=)
+end
+
+it 'allow withdraw if account has enough balance' do
+  expected_output = {:status => true, :message => 'success', :date => Date.today, :amount => 45}
+  expect(subject.withdraw(45, account)).to eq expected_output
+
+end
+
+
 end
